01f828050e7879c6944a319fb15447aacb317e96
Kayode <kayodefredrickbalogun@gmail.com>
2024-07-01 11:21:48 +0100

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Balogun Kayode on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
